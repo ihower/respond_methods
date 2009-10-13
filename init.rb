d2358effc3f75699693313c7bf7aa23c4c4a4392
Wen-Tien Chang <ihower@gmail.com>
2009-10-13 16:32:58 +0800

    remove amesome features

--- init.rb
+++ init.rb
@@ -1,1 +1,4 @@
-require File.join(File.dirname(__FILE__), 'lib', 'inherited_resources')
+unless defined?(ActionController::Responder)
+  require File.join(File.dirname(__FILE__), 'lib', 'responder')
+  require File.join(File.dirname(__FILE__), 'lib', 'respond_to')
+end
